a4129cf29da8cc8551f02cc744e8126faaa35b36
Anthony Arnold <anthony.arnold@uqconnect.edu.au>
2021-09-09 16:23:15 +1000

    Update day01.f

--- day01.f
+++ day01.f
@@ -1,10 +1,25 @@
+      function fuel(mass)
+        integer mass, fuel
+        fuel = 0
+
+ 98     mass = mass / 3 - 2
+        if (mass .lt. 1) go to 99
+        fuel = fuel + mass
+        go to 98
+        
+ 99     return
+      end function
+      
       program day01
-          integer x, tot
-          tot = 0
+          integer x, part1, part2, fuel
+          part1 = 0
+          part2 = 0
 
  1        read (*,*,END=2) x
-          tot = tot + x / 3 - 2
+          part1 = part1 + x / 3 - 2
+          part2 = part2 + fuel(x)
           goto 1
- 2        write(*,*) tot    
+
+ 2        write(*,*) part1, part2
       stop
       end
