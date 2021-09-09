49b608959a52ec614d4390065f21b6fdbade4f44
Anthony Arnold <anthony.arnold@uqconnect.edu.au>
2021-09-09 17:26:12 +1000

    Update intcode.f

--- intcode.f
+++ intcode.f
@@ -1,23 +1,23 @@
-      subroutine intcode(n)
-        integer n(*), pp, op, lhs, rhs, dst
-        pp = 1
+      subroutine intcode(mem)
+        integer mem(*), ip, op, lhs, rhs, dst
+        ip = 1
 
- 1      op = n(pp)
+ 1      op = mem(ip)
 
         if (op.eq.99) return
 
 c Intcode positions are 0-based
-        lhs = n(n(pp + 1) + 1)
-        rhs = n(n(pp + 2) + 1)
-        dst = n(pp + 3) + 1
+        lhs = mem(mem(ip + 1) + 1)
+        rhs = mem(mem(ip + 2) + 1)
+        dst = mem(ip + 3) + 1
 
         if (op.eq.1) then
-          n(dst) = lhs + rhs
+          mem(dst) = lhs + rhs
         end if
         if (op.eq.2) then
-          n(dst) = lhs * rhs
+          mem(dst) = lhs * rhs
         end if
 
-        pp = pp + 4
+        ip = ip + 4
         go to 1
       end
