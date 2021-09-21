50b5185c10f61eb6cb556bfb2ebfd2d58dd260e5
Anthony Arnold <anthony.arnold@uqconnect.edu.au>
2021-09-21 12:39:01 +1000

    Finish day 16

--- day16.f
+++ day16.f
@@ -2,8 +2,8 @@
         implicit none
         character*650 dat
         integer input(650), output(650)
-        integer input2(6 500 000), output2(6 500 000)
-        integer i, j, n, offset
+        integer input2(650 * 10 000)
+        integer i, j, n, offset, tot
 
         offset = 0
         i = 0
@@ -17,24 +17,33 @@
         end if
         n = i
 
-        do 10 i = 1,10 000
+        do 10 i = 0,9 999
            do 11 j = 1,n
-              input2(i * j) = input(j)
+              input2(i*n + j) = input(j)
  11        continue
  10     continue
 
         call fft(input, output, n, 0)
-        do 20 i = 1,8
-           dat(i:i) = char(input(i) + ichar('0'))
- 20     continue
-        write(*,*) dat(1:8)
+        call output8(input, 0)
 
-c        call fft(input2, output2, n * 10 000, offset)
-c        do 30 i = 1,8
-c           dat(i:i) = char(input2(i+offset) + ichar('0'))
-c 30     continue
-c        write(*,*) dat(1:8)
+        do 24 j = 1,100
+            tot = 0
+            do 25 i = n*10 000,offset+1,-1
+                tot = mod(tot + input2(i), 10)
+                input2(i) = tot
+ 25         continue
+ 24     continue
+        call output8(input2, offset)
       stop
+      end
+
+      subroutine output8(arr, offset)
+        integer i, offset, arr(offset + 8)
+        character*8 dat
+        do 30 i = 1,8
+           dat(i:i) = char(arr(i+offset) + ichar('0'))
+ 30     continue
+        write(*,*) dat
       end
 
       subroutine fft(input, output, n, offset)
